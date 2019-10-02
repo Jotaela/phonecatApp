a9c49e292d0ab256af7b1330c9479681304e7d60
Joan Lluís <dijlluis@DISI.LOCAL>
2019-10-02 10:01:24 +0200

    WIP

--- app/app.js
+++ app/app.js
@@ -2,7 +2,12 @@ angular.module('phonecatApp', ['ui.router']).
     config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider.state('home', {
             url: '/home',
-            templateUrl: 'phone-list.template.html',
-            controller: 'PhoneListController'
-        })
+            views: {
+                '': { templateUrl: 'phone-list.template.html', controller: 'PhoneListController' },
+                'vista1@home': { template: '<h1>Vista 1</h1>' },
+                'vista2@home': { template: '<h1>Vista 2</h1>' }
+
+            }
+        });
+        $urlRouterProvider.otherwise('/home');
     });
